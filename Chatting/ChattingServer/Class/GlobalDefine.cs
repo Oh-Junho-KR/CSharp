00d21141815a6e62ef41571bda4719fd082e73ff
Oh-Junho-KR <81357906+Oh-Junho-KR@users.noreply.github.com>
2024-10-29 09:49:14 +0900

    Create GlobalDefine.cs
    전역 변수 정의

---
+++ Chatting/ChattingServer/Class/GlobalDefine.cs
@@ -0,0 +1,23 @@
+namespace Chatting
+{
+    internal static class GlobalDefine
+    {
+        public enum eProtocol
+        {
+            None        = 000,
+            Login       = 100,
+            Logout      = 101,
+            EnterRoom   = 200,
+            OutRoom     = 201,
+            EnterChat   = 300,
+            ExitChat    = 301,
+            SendMsg     = 400,
+            RecvMsg     = 401
+        }
+
+        public static string sServerIP = "127.0.0.1";
+        public static int nServerPort = 10001;
+        public static int nListen = 10;
+        public static int nByte = 1024;
+    }
+}
