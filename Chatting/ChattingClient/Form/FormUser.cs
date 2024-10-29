5d8c2b9db5936852d054633f75ec0ce64a789ef0
Oh-Junho-KR <81357906+Oh-Junho-KR@users.noreply.github.com>
2024-10-29 09:43:07 +0900

    Create FormUser.cs
    화면 - 유저 리스트

---
+++ Chatting/ChattingClient/Form/FormUser.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChattingClient
+{
+    public partial class FormUser : UserControl
+    {
+        //public delegate void EnterChatEventHandler(string sID, string sTitle, string sIP, string sPort, string sName);
+        //public EnterChatEventHandler EnterChatEvent;
+
+        public FormUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
